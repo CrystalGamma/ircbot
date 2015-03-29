2264750ff2070507154062063d8818eb565c9089
Jona Stubbe (CrystalGamma) <dev@jona-stubbe.de>
2015-03-29 11:55:13 +0200

    add filesystem-backed JsonJoinDB

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate rand;
 use rand::Rng;
 extern crate hyper;
 extern crate rustc_serialize as serialize;
-use serialize::json::Json;
+use serialize::json::{Json, ToJson};
 extern crate chrono;
 use chrono::offset::TimeZone;
 extern crate regex;
@@ -18,6 +18,7 @@ use std::sync::mpsc::{channel, Sender, Receiver};
 use std::str::Pattern;
 use std::sync::Arc;
 use std::collections::HashMap;
+use std::path::Path;
 
 fn exchange<T>(a: &mut T, mut b: T) -> T {
 	std::mem::swap(a, &mut b);
@@ -458,9 +459,35 @@ fn handle_msg(msg: irc::IrcMessage, ctx: &mut BotContext) -> bool {
 
 #[derive(Clone)]
 struct JoinUser {
-	num_visits: u32,
+	num_visits: u64,
 	last_visit: chrono::DateTime<chrono::Local>,
 	first_visit: chrono::DateTime<chrono::Local>
+}
+
+macro_rules! tryopt{
+	($e:expr) => (match $e {Some(x) => x, _ => return None})
+}
+
+impl JoinUser {
+	fn from_json(json: &Json) -> Option<JoinUser> {
+		let obj = tryopt!(json.as_object());
+		let num = tryopt!(obj.get("num_visits").and_then(|json|json.as_u64()));
+		let first = tryopt!(obj.get("first_visit").and_then(|json|json.as_string()));
+		let last = tryopt!(obj.get("last_visit").and_then(|json|json.as_string()));
+		let first_visit = tryopt!(first.parse().ok());
+		let last_visit = tryopt!(last.parse().ok());
+		Some(JoinUser {num_visits: num, first_visit: first_visit, last_visit: last_visit})
+	}
+}
+
+impl ToJson for JoinUser {
+	fn to_json(&self) -> Json {
+		let mut obj = serialize::json::Object::new();
+		obj.insert("num_visits".to_string(), Json::U64(self.num_visits));
+		obj.insert("first_visit".to_string(), Json::String(self.first_visit.to_rfc3339()));
+		obj.insert("last_visit".to_string(), Json::String(self.last_visit.to_rfc3339()));
+		Json::Object(obj)
+	}
 }
 
 enum JoinStatus{
@@ -468,17 +495,30 @@ enum JoinStatus{
 	Alias(String)
 }
 
+impl ToJson for JoinStatus {
+	fn to_json(&self) -> Json {
+		let mut obj = serialize::json::Object::new();
+		match self {
+			&JoinStatus::User(ref user) => obj.insert("user".to_string(), user.to_json()),
+			&JoinStatus::Alias(ref text) => obj.insert("alias".to_string(), Json::String(text.clone()))
+		};
+		Json::Object(obj)
+	}
+}
+
 struct MemJoinDB(HashMap<String, JoinStatus>);
 
 impl MemJoinDB {
 	pub fn new() -> MemJoinDB {MemJoinDB(HashMap::new())}
-	fn fetch(&self, name: &str) -> Option<&JoinUser> {
+}
+impl FetchUpJoinDB for MemJoinDB {
+	fn fetch(&self, name: &str) -> Option<JoinUser> {
 		match self.0.get(name) {
 			Some(&JoinStatus::Alias(ref name2)) => match self.0.get(name) {
-				Some(&JoinStatus::User(ref u)) => Some(u),
+				Some(&JoinStatus::User(ref u)) => Some(u.clone()),
 				_ => None
 			},
-			Some(&JoinStatus::User(ref u)) => Some(u),
+			Some(&JoinStatus::User(ref u)) => Some(u.clone()),
 			None => None
 		}
 	}
@@ -497,10 +537,15 @@ impl MemJoinDB {
 	}
 }
 
+trait FetchUpJoinDB {
+	fn fetch(&self, name: &str) -> Option<JoinUser>;
+	fn update(&mut self, name: &str, new: JoinStatus) -> String;
+}
+
 trait JoinDB {
 	fn join(&mut self, name: &str) -> (String, Option<JoinUser>);
 }
-impl JoinDB for MemJoinDB {
+impl<DB: FetchUpJoinDB> JoinDB for DB {
 	fn join(&mut self, name: &str) -> (String, Option<JoinUser>) {
 		let now = chrono::Local::now();
 		let (is_new, user) = self.fetch(name).map(|s|(false, s.clone())).unwrap_or_else(
@@ -511,7 +556,33 @@ impl JoinDB for MemJoinDB {
 	}
 }
 
-fn ordinal(i: u32) -> &'static str {
+struct JsonJoinDB(String);
+
+impl JsonJoinDB {
+	pub fn new(dir: String) -> JsonJoinDB {JsonJoinDB(dir)}
+}
+impl FetchUpJoinDB for JsonJoinDB {
+	fn fetch(&self, name: &str) -> Option<JoinUser> {
+		let mut file = tryopt!(std::fs::File::open(Path::join(&Path::new(&self.0), &Path::new(name))).ok());
+		let mut s = String::new();
+		file.read_to_string(&mut s).unwrap();
+		let json = Json::from_str(&s).unwrap();
+		let obj = json.as_object().unwrap();
+		if let Some(user) = obj.get("user") {
+			Some(JoinUser::from_json(user).unwrap())
+		} else {
+			None //Some(JoinStatus::Alias(obj.get("alias").unwrap().as_string().unwrap().to_string()))
+		}
+	}
+	fn update(&mut self, name: &str, new: JoinStatus) -> String {
+		use std::path::Path;
+		let mut file = std::fs::File::create(Path::join(&Path::new(&self.0), &Path::new(name))).unwrap();	// FIXME: will overwrite aliases
+		write!(&mut file, "{}", new.to_json()).unwrap();
+		name.to_string()
+	}
+}
+
+fn ordinal(i: u64) -> &'static str {
 	match i % 10 {
 		1 if i != 11 => "st",
 		2 if i != 12 => "nd",
@@ -557,7 +628,7 @@ fn bot(cfg: BotConfig) {
 		joins_tx: joins_tx
 	};
 	thread::spawn(move ||{
-		let mut joins = MemJoinDB::new();
+		let mut joins = JsonJoinDB::new("joinlog".to_string());
 		while let Ok(nick) = joins_rx.recv() {
 			match joins.join(&nick) {
 				(nick, Some(user)) => {
